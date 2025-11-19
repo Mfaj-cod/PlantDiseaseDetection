1e6da86b0810135d4219bc1732f89c5491866a63
M. fajal <fajalmoaj@gmail.com>
2025-11-19 16:39:55 +0530

    _

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 
 # model architecture
-num_classes = 38  #the number of classes
+num_classes = 38
 model, _, _ = create_model_loss_optim(num_classes)
 
 # Loading saved weights
@@ -43,15 +43,15 @@ transform = transforms.Compose([
 # label mapping
 class_names = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
+# For displaying unique class names without duplicates and underscores
 unique_class_names = ['Apple', 'Blueberry', 'Cherry (including sour)', 'Corn (maize)', 'Grape', 'Orange', 'Peach', 'Pepper, bell', 'Potato', 'Raspberry', 'Soybean', 'Squash', 'Strawberry', 'Tomato']
 
-# Loading environment variables from .env file
-load_dotenv()
 
+
+load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME")
 gemini_model = None
-
 
 
 
@@ -108,7 +108,7 @@ def get_disease_remedy(disease_name: str):
             logger.warning("Gemini returned an empty remedy for %s.", disease_name)
             return "No remedy suggestion returned by Gemini."
         
-        # Convert **text** to <strong>text</strong>
+        # Converting **text** to <strong>text</strong>
         parts = text.split("**")
         formatted_text = ""
         toggle = True
