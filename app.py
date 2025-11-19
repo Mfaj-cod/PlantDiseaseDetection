7570bb94046f7de70a79355550180a2450e3774b
M. fajal <fajalmoaj@gmail.com>
2025-11-19 16:27:30 +0530

    showing the capability of the model on distinct plants

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ transform = transforms.Compose([
 # label mapping
 class_names = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
+unique_class_names = ['Apple', 'Blueberry', 'Cherry (including sour)', 'Corn (maize)', 'Grape', 'Orange', 'Peach', 'Pepper, bell', 'Potato', 'Raspberry', 'Soybean', 'Squash', 'Strawberry', 'Tomato']
 
 # Loading environment variables from .env file
 load_dotenv()
@@ -130,21 +131,21 @@ def get_disease_remedy(disease_name: str):
 def upload_predict():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return render_template('index.html', prediction="No file uploaded.", remedy=None)
+            return render_template('index.html', prediction="No image uploaded.", remedy=None, classes=unique_class_names)
         
         file = request.files['file']
         if not file:
-            return render_template('index.html', prediction="No image selected.", remedy=None)
+            return render_template('index.html', prediction="No image selected.", remedy=None, classes=unique_class_names)
         
         img_bytes = file.read()
         prediction = predict_image(img_bytes).replace('_', ' ')
         logger.info(f"Image prediction: {prediction}")
 
         remedy = get_disease_remedy(prediction)
-        return render_template('index.html', prediction=prediction, remedy=remedy)
+        return render_template('index.html', prediction=prediction, remedy=remedy, classes=unique_class_names)
     
     # logger.info("problem in rendering uploaded page.")
-    return render_template('index.html', prediction=None, remedy=None)
+    return render_template('index.html', prediction=None, remedy=None, classes=unique_class_names)
 
 
 
@@ -153,13 +154,13 @@ def upload_predict():
 def capture_predict():
     file = request.files.get('file')
     if not file:
-        return render_template('index.html', prediction="No image captured.", remedy=None)
+        return render_template('index.html', prediction="No image captured.", remedy=None, classes=unique_class_names)
     
     prediction = predict_image(file.read()).replace('_', ' ')
     logger.info(f"Captured image prediction: {prediction}")
     
     remedy = get_disease_remedy(prediction)
-    return render_template('index.html', prediction=prediction, remedy=remedy)
+    return render_template('index.html', prediction=prediction, remedy=remedy, classes=unique_class_names)
 
 
 
